7917620e2ef06ef3cf8a907096d15ad1feaab90f
furofo <chriscellphonerepair@gmail.com>
2021-10-04 00:36:53 -0400

    full functionaliry of mobile responsive menu example version

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
-    var x = document.getElementById("myTopnav");
+    console.log("function clicked");
+    let x = document.getElementById("myTopnav");
+    console.log(x);
     if (x.className === "topnav") {
       x.className += " responsive";
+      console.log(x);
     } else {
       x.className = "topnav";
     }
