acb9cdd382af9d4046ce5ea2f862ea17fd5489b7
furofo <chriscellphonerepair@gmail.com>
2021-10-04 23:33:59 -0400

    first version of potrait view mobile menu with javascript styling for height

--- script.js
+++ script.js
@@ -10,3 +10,14 @@ function myFunction() {
       x.className = "topnav";
     }
   }
+
+  window.onload = function() {
+    //set height of mobile restaruant logo to div containing fa fa bars
+    let mobileLogoPic = document.getElementsByClassName("mobile-logo");
+    let fafaBars = document.getElementsByClassName("icon");
+    console.log(mobileLogoPic[0]);
+    console.log(fafaBars[0]);
+    console.log(fafaBars[0].clientHeight);
+    // leave some room for some padding simple math simple dazoooo
+    mobileLogoPic[0].style.height = fafaBars[0].clientHeight - 20 + "px";
+  }
